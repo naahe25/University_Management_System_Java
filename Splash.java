f0f75d76a55a323a072be55fe1389bfe0496880d
Md.Naahe Uddin Laskar <123208141+naahe25@users.noreply.github.com>
2025-07-29 14:35:26 +0600

    Update Splash.java
    Removed the image of MU at start of application

--- Splash.java
+++ Splash.java
@@ -1,42 +1,7 @@
-import java.awt.Image;
-import javax.swing.*;
-
-public class Splash extends JFrame implements Runnable {
-
-    Thread t;
-
-    Splash() {
-        // For Adding Image
-        ImageIcon img1 = new ImageIcon(ClassLoader.getSystemResource("Icons/Metro_Pic.jpg"));
-        Image img2 = img1.getImage().getScaledInstance(1000, 700, Image.SCALE_DEFAULT);
-        ImageIcon img3 = new ImageIcon(img2);
-        JLabel image = new JLabel(img3);
-        add(image);
-
-        // Set fixed size and center the window
-        setSize(1000, 700);
-        setLocationRelativeTo(null); // center the frame on screen
-        setVisible(true);
-
-        // Start the thread
-        t = new Thread(this);
-        t.start();
-    }
-
-    // run method for the thread
-    public void run() {
-        try {
-            Thread.sleep(3000); // keep splash visible for 3 seconds
-            setVisible(false);
-
-            // Next Frame
-            new Login(); // make sure Login class exists
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+public class Splash {
 
     public static void main(String[] args) {
-        new Splash();
+        // Directly open the Login window
+        new Login();
     }
 }
